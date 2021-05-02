80b55dbc96e17f3d3f7dd14af595ac5a69b9d131
Björn Johansson <bjorn.ki.johansson@gmail.com>
2021-05-02 16:35:10 +0200

    Added some prototyping code, need to generalize and add tests.

--- outlier.py
+++ outlier.py
@@ -52,4 +52,48 @@ if __name__ == "__main__":
     for items in ordered_list_with_files(file_occurence)[0:top_churners]:
         print(f"{str(items[1]):8}{items[0]:10}")
 
+    data = {"filename": {"Churn": 1, "Complexity": 20}, "filename2": {"Churn": 5, "Complexity": 20},
+            "yada": {"Churn": 20, "Complexity": 1}, "yada2": {"Churn": 15, "Complexity": 15}, "yada5": {"Churn": 15, "Complexity": 15}, "yada20": {"Churn": 1, "Complexity": 1}}
+    print(data)
+    print(data["filename"]["Churn"])
+
+    maxChurn = 0
+    maxComplexity = 0
+    for value in data.values():
+        if value["Churn"] > maxChurn:
+            maxChurn = value["Churn"]
+        if value["Complexity"] > maxComplexity:
+            maxComplexity = value["Complexity"]
+    max_yval = 20
+    discretization_yval = 1 / max_yval
+    max_xval = 40
+    discretization_xval = 1 / max_xval
+
+    points_to_plot = dict()
+    for y_val in range(max_yval, -1, -1):
+        points_to_plot[y_val] = None
+
+    for value in data.values():
+        discreatized_yval = round(value["Churn"] / maxChurn * max_yval)
+        discreatized_xval = round(value["Complexity"] / maxComplexity * max_xval)
+        if points_to_plot[discreatized_yval] is None:
+            points_to_plot[discreatized_yval] = [discreatized_xval]
+        else:
+            points_to_plot[discreatized_yval] = [points_to_plot[discreatized_yval], discreatized_xval]
+    print(points_to_plot)
+    print("\n\n***************************************************\n\nChurn vs Complexity diagram \n")
+    print("Churn")
+    for y_val in range(max_yval, -1, -1):
+        print("|", end = '')
+        if points_to_plot[y_val] is not None:
+            for x_val in range(0, max_xval + 1, 1):
+                if x_val in points_to_plot[y_val]:
+                    print("X", end = '')
+                else:
+                    print(" ", end='')
+        print("")
+
+    for x_val in range(0, max_xval + 1, 1):
+        print("-", end='')
+    print(" Complexity")
     # See PyCharm help at https://www.jetbrains.com/help/pycharm/
